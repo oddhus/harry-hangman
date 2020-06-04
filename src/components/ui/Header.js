9817220fdecfe3fb77d9bbe2fcef2c331f2d8955
oddhus <huseby.oddmund@gmail.com>
2020-06-04 19:46:56 +0200

    Added header comp

---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import AppBar from '@material-ui/core/AppBar'
+
+function Header() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
