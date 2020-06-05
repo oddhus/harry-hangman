4eedc16355f3345955c46d80388fb7382a4b53d1
oddhus <huseby.oddmund@gmail.com>
2020-06-05 15:23:08 +0200

    header completed

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles/'
-import { AppBar, Toolbar, Button, useScrollTrigger, Tabs, Tab } from '@material-ui/core'
+import { AppBar, Toolbar, Button, useScrollTrigger, Tabs, Tab, useMediaQuery, IconButton, SwipeableDrawer, List, ListItem, ListItemText, useTheme } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import { Link } from 'react-router-dom'
 
 import logo from '../../assets/HarryHangmanLogo.svg'
-import { Link } from 'react-router-dom'
 
 function ElevationScroll(props) {
   const { children } = props
@@ -19,6 +20,34 @@ function ElevationScroll(props) {
 }
 
 const useStyles = makeStyles(theme => ({
+  appbar: {
+    zIndex: theme.zIndex.modal + 1
+  },
+  drawer: {
+    backgroundColor: theme.palette.common.hpRed
+  },
+  drawerIcon:{
+    height: "40px",
+    width: "40px"
+  },
+  drawerIconContainer: {
+    marginLeft: "auto",
+    "&:hover": {
+      backgroundColor: "transparent"
+    }
+  },
+  drawerItem: {
+    color: "white",
+    opacity: 0.7
+  },
+  drawerItemSelected: {
+    "&.Mui-selected": {
+      backgroundColor: theme.palette.common.hpGold,
+    },
+    "& .MuiListItemText-root": {
+      opacity: 1
+    }    
+  },
   logo: {
     height: "8em",
     [theme.breakpoints.down("md")]: {
@@ -68,18 +97,26 @@ const useStyles = makeStyles(theme => ({
 }))
 
 const routes = [
-  {name: "Play", link: "/"},
-  {name: "LeaderBoard", link: "/leaderboard"},
-  {name: "About", link: "/about"},
+  {name: "Play", link: "/", activeIndex: 0},
+  {name: "LeaderBoard", link: "/leaderboard", activeIndex: 1},
+  {name: "About", link: "/about", activeIndex: 2},
 ]
-
-
 
 function Header() {
   const classes = useStyles()
+  const theme = useTheme()
+  const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent)
+  const matches = useMediaQuery(theme.breakpoints.down("md"))
 
   const [value, setValue] = useState(0)
+  const [openDrawer, setOpenDrawer] = useState(false)
 
+  useEffect(() => {
+    const pathname = window.location.pathname;
+    const valueIndex = routes.findIndex(option => option.link === pathname);
+
+    setValue(valueIndex === -1 ? 1 : valueIndex);
+  }, []);
 
   const tabs = (
     <React.Fragment>
@@ -102,15 +139,51 @@ function Header() {
     </React.Fragment>
   )
 
+  const drawer = (
+    <React.Fragment>
+      <SwipeableDrawer disableBackdropTransition={!iOS} disableDiscovery={iOS}
+      classes={{paper: classes.drawer}} 
+      onClose={() => setOpenDrawer(false)}
+      onOpen={() => setOpenDrawer(true)}
+      open={openDrawer}
+      >
+        <div className={classes.toolbarMargin}/>
+        <List disablePadding>
+          {routes.map(route => (
+            <ListItem 
+            button
+            classes={{selected: classes.drawerItemSelected}}
+            component={Link} 
+            divider 
+            key={route.link}
+            onClick={() => {setOpenDrawer(false); setValue(route.activeIndex)}} 
+            selected={value === route.activeIndex} 
+            to={route.link}
+          >
+            <ListItemText
+              className={classes.drawerItem} 
+            >
+            {route.name}
+            </ListItemText>
+          </ListItem>
+          ))}
+        </List>
+      </SwipeableDrawer>
+      <IconButton className={classes.drawerIconContainer} onClick={() => setOpenDrawer(!openDrawer)} disableRipple>
+        <MenuIcon className={classes.drawerIcon}/>
+      </IconButton>
+    </React.Fragment>
+  )
+
   return (
     <React.Fragment>
       <ElevationScroll>
-        <AppBar position="fixed">
+        <AppBar position="fixed" className={classes.appbar}>
           <Toolbar disableGutters>
             <Button component={Link} to="/" className={classes.logoContainer}>
               <img src={logo} alt="Harry Hangman Logo" className={classes.logo} />
             </Button>
-            {tabs}
+            {matches ? drawer : tabs}
           </Toolbar>
         </AppBar>
       </ElevationScroll>
