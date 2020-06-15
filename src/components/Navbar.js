ab5fbf183b8a96bd16ee58493d27c01fe5e66052
oddhus <huseby.oddmund@gmail.com>
2020-06-15 19:44:42 +0200

    removed imports

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import { Grid, Button, Box } from '@material-ui/core'
 import PlayerBar from './PlayerBar'
 import { useStore } from '../store/store'
--- src/components/StatusMessage.js
+++ src/components/StatusMessage.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
 import { makeStyles } from '@material-ui/core/styles';
 import { useStore } from '../store/store';
-import { observer, useObserver } from 'mobx-react-lite';
+import { useObserver } from 'mobx-react-lite';
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
@@ -21,26 +21,13 @@ const useStyles = makeStyles((theme) => ({
 export default function CustomizedSnackbars(props) {
     const classes = useStyles();
     const { game } = useStore()
-    const [open, setOpen] = useState(false)
-    const [success, setSuccess] = useState(null)
-
-    useEffect(() => {
-        if(props.success){
-            if (props.success === "added") {
-                setSuccess(true)
-                setOpen(true)
-            } else if (props.success.length !== 0) {
-                setSuccess(false)
-                setOpen(true)
-            }
-        }
-    }, [props.success])
 
     const handleClose = (event, reason) => {
         if (reason === 'clickaway') {
             return;
         }
-        setOpen(false);
+        game.isAdded = false
+        game.errorMessage = null
     };
 
     const successMessage = (
